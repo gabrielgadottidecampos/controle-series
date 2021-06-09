c79390c14f949d841299e9de8baf515561481730
Gabriel Gadotti <gabriel-gadotti@hotmail.com>
2021-06-09 16:32:19 -0300

    versão 1.0

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SeriesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/series', [SeriesController::class, 'index']);
