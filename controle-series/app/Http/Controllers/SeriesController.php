bcb11c5d2c9da17137acc7498c8c9fc7a53e4c6b
Gabriel Gadotti <gabriel-gadotti@hotmail.com>
2021-06-09 16:32:37 -0300

    versão 1.0

---
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series =['Grey\`s Anatomy','Lost','Agents of Shield'];
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+        return $html;
+    }
+}
